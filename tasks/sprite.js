ac70043afc146cc3fb3476c42f85c0b1a366b471
laoshu133 <admin@laoshu133.com>
2014-06-22 15:59:31 +0800

    初步实现 image-set

--- tasks/sprite.js
+++ tasks/sprite.js
@@ -8,6 +8,9 @@ module.exports = function (grunt) {
 
 	var SLICE_PLACE = '{{$slice_{id}$}}';
 	var R_SLICE_PLACE = /\{\{\$slice_(\d+)\$\}\}/g;
+	var IMAGE_SET_PLACE = '/*image-set-place*/';
+	var IMAGE_SET_CSS_TMPL = 'background-image: -webkit-image-set(url({spriteImg}) 1x, url({retinaSpriteImg}) 2x); background-image: -moz-image-set(url({spriteImg}) 1x, url({retinaSpriteImg}) 2x); background-image: -ms-image-set(url({spriteImg}) 1x, url({retinaSpriteImg}) 2x); background-image: image-set(url({spriteImg}) 1x, url({retinaSpriteImg}) 2x);';
+	var MEDIA_QUERY_CSS_TMPL = '\n\n/* {imgDest} */\n@media only screen and (-o-min-device-pixel-ratio: 3/2), only screen and (min--moz-device-pixel-ratio: 1.5), only screen and (-webkit-min-device-pixel-ratio: 1.5), only screen and (min-resolution: 240dpi), only screen and (min-resolution: 2dppx) {\n  {cssText}\n}\n';
 
 	function fixPath(path) {
 		return String(path).replace(/\\/g, '/').replace(/\/$/, '');
@@ -39,7 +42,7 @@ module.exports = function (grunt) {
 			}
 
 			var currCssInx = cssHash[css];
-			if(currCssInx == null) {
+			if(currCssInx === void 0) {
 				currCssInx = cssHash[css] = cssInx;
 
 				cssList[cssInx++] = {
@@ -105,7 +108,12 @@ module.exports = function (grunt) {
 			// 默认使用二叉树最优排列算法
 			algorithm: 'binary-tree',
 			// 默认使用`pngsmith`图像处理引擎
-			engine: 'pngsmith'
+			engine: 'pngsmith',
+
+			// 扩展参数，不建议修改，image-set 模板
+			IMAGE_SET_CSS_TMPL: IMAGE_SET_CSS_TMPL,
+			// 扩展参数，不建议修改， media query 模板
+			MEDIA_QUERY_CSS_TMPL: MEDIA_QUERY_CSS_TMPL
 		});
 
 		// `padding` must be even
@@ -127,6 +135,27 @@ module.exports = function (grunt) {
 			}
 
 			async.waterfall([
+				// base config
+				function baseConfig(cb) {
+					var 
+					cssFilename = path.basename(src, '.css'),
+					timeNow = grunt.template.today('yyyymmddHHmmss');
+
+					if(options.newsprite) {
+		                cssFilename += '-' + timeNow;
+		            }
+
+					sliceData.timestamp = options.spritestamp ? ('?'+timeNow) : '';
+					sliceData.imgDest = path.join(options.spritedest, cssFilename + '.png');
+					sliceData.spriteImg = path.join(options.spritepath, cssFilename + '.png') + 
+						sliceData.timestamp;
+
+					sliceData.retinaImgDest = sliceData.imgDest.replace(/\.png$/, '@2x.png');
+					sliceData.retinaSpriteImg = path.join(options.spritepath, cssFilename + '@2x.png') + 
+						sliceData.timestamp;
+
+					cb(null);
+				},
 				// create sprite image
 				function createSpriteImg(cb) {
 					createSprite(sliceData.imgList, options, cb);
@@ -134,59 +163,50 @@ module.exports = function (grunt) {
 				// write sprite image file
 				function writeSrpiteFile(spriteImgData, cb) {
 		            // cssFilename, timestamp
-		            var 
-		            cssFilename = path.basename(src, '.css'),
-		            timeNow = grunt.template.today('yyyymmddHHmmss');
+		            var cssFilename = path.basename(src, '.css');
 
-		            if(options.newsprite){
+		            if(options.newsprite) {
 		                cssFilename += '-' + timeNow;
 		            }
 
-		            sliceData.cssFilename = cssFilename;
-		            sliceData.timestamp = options.spritestamp ? ('?'+timeNow) : '';
-
 		            // write file
-		            var imgDest = sliceData.imgDest = path.join(options.spritedest, cssFilename + '.png');
+	                grunt.file.write(sliceData.imgDest, spriteImgData.image, { encoding: 'binary' });
+	                grunt.log.writelns(('Done! [Created] -> ' + sliceData.imgDest));
 
-		            sliceData.destImgStamp = options.spritestamp ? '?' + timeNow : '';
-
-	                grunt.file.write(imgDest, spriteImgData.image, { encoding: 'binary' });
-	                grunt.log.writelns(('Done! [Created] -> ' + imgDest));
-
+	                sliceData.spriteImgData = spriteImgData;
 	                cb(null, spriteImgData.coordinates);
 				},
 				// set slice position
 				function setSlicePosition(coords, cb) {
-					var 
-					rsemicolon = /;\s*$/,
-					spriteFilename = path.basename(sliceData.imgDest),
-					spriteImg = path.join(options.spritepath + spriteFilename) + sliceData.destImgStamp;
+					var rsemicolon = /;\s*$/;
 
 					sliceData.cssList.forEach(function(cssItem) {
 						var 
 						css = cssItem.css,
 						coord = coords[cssItem.imgFullPath];
 
-						css = css.replace(cssItem.imgPath, spriteImg);
+						css = css.replace(cssItem.imgPath, sliceData.spriteImg);
 
 						// Add a semicolon if needed
 	                    if(!rsemicolon.test(css)){
 	                        css += ';';
 	                    }
-						css += ' background-position:-'+ coord.x +'px -'+ coord.y +'px;';
+	                    css += IMAGE_SET_PLACE;
+						css += 'background-position:-'+ coord.x +'px -'+ coord.y +'px;';
 
 						cssItem.newCss = css;
+						cssItem.height = coord.height;
+						cssItem.width = coord.width;
+						cssItem.x = coord.x;
+						cssItem.y = coord.y;
 					});
 
 					cb(null);
 				},
-				// get retina image
+				// get retina image & add image-set css
 				function getRetinaImg(cb) {
-					if(options.useimageset) {
-						return cb(null, null);
-					}
-
 					var 
+					useimageset = options.useimageset,
 					retinaImgList = sliceData.retinaImgList = [],
 					retinaImgHash = sliceData.retinaImgHash = {};
 
@@ -196,16 +216,35 @@ module.exports = function (grunt) {
 						filename = path.basename(cssItem.imgPath, extName),
 						retinaImgFullPath = path.join(path.dirname(cssItem.imgFullPath), filename + '@2x' + extName);
 
-						if(retinaImgHash[retinaImgFullPath] == null && grunt.file.exists(retinaImgFullPath)) {
-							retinaImgHash[retinaImgFullPath] = id;
+						if(!retinaImgHash[retinaImgFullPath] && grunt.file.exists(retinaImgFullPath)) {
 							cssItem.retinaImgFullPath = retinaImgFullPath;
+							retinaImgHash[retinaImgFullPath] = {
+								id: id,
+								height: 2 * cssItem.height,
+								width: 2 * cssItem.width,
+								x: 2 * cssItem.x,
+								y: 2 * cssItem.y
+							};
 
 							retinaImgList.push(retinaImgFullPath);
 						}
+
+						// add image-set css
+						var imageSetCSS = useimageset ? 
+						//var imageSetCSS = useimageset && retinaImgHash[retinaImgFullPath] ? 
+							options.IMAGE_SET_CSS_TMPL.replace(/\{spriteImg\}/g, sliceData.spriteImg)
+								.replace(/\{retinaSpriteImg\}/g, sliceData.retinaSpriteImg) :
+							'';
+						cssItem.newCss = cssItem.newCss.replace(IMAGE_SET_PLACE, imageSetCSS);
 					});
 
 					if(retinaImgList.length > 0) {
-						return createSprite(retinaImgList, options, cb);
+						if(!useimageset) {
+							return createSprite(retinaImgList, options, cb);
+						}
+
+						var retinaImageCreater = require('../lib/retinaImageCreater');
+						return retinaImageCreater.createBySliceData(sliceData, options, cb);
 					}
 
 					cb(null, null);
@@ -215,9 +254,7 @@ module.exports = function (grunt) {
 					if(retinaSpriteImgData) {
 						sliceData.retinaSpriteImgData = retinaSpriteImgData;
 
-						var retinaImgDest = sliceData.imgDest.replace(/\.png$/, '@2x.png');
-
-						sliceData.retinaImgDest = retinaImgDest;
+						var retinaImgDest = sliceData.retinaImgDest;
 
                         grunt.file.write(retinaImgDest, retinaSpriteImgData.image, { encoding: 'binary' });
                         grunt.log.writelns(('Done! [Created] -> ' + retinaImgDest));
@@ -229,6 +266,7 @@ module.exports = function (grunt) {
 				function replaceCss(cb) {
 					var 
 					cssList = sliceData.cssList,
+					useimageset = options.useimageset,
 					retinaSpriteImgData = sliceData.retinaSpriteImgData,
 					coords = retinaSpriteImgData ? retinaSpriteImgData.coordinates : {};
 
@@ -251,7 +289,7 @@ module.exports = function (grunt) {
 						cssItem = cssList[parseInt(id, 10)],
 						ret = cssItem ? cssItem.newCss : '';
 
-						if(!cssItem) {
+						if(!cssItem || useimageset) {
 							return ret;
 						}
 
@@ -260,10 +298,11 @@ module.exports = function (grunt) {
 							return ret;
 						}
 
+						place = SLICE_PLACE.replace('{id}', id);
+
 						// media query retina css
 						var 
 						selector,
-						place = SLICE_PLACE.replace('{id}', id),
 						rselector = new RegExp('([^}\\n\\/]+)\\{[^\\}]*?' + place.replace(rreEscape, '\\$&'));
 						tmpCss =  tmpCss.replace(rselector, function(a, b) {
 							selector = b;
@@ -293,23 +332,18 @@ module.exports = function (grunt) {
 						return ret;
 					});
 
-					if(retinaSpriteImgData) {
+					if(retinaSpriteImgData && !useimageset) {
 						var 
-						spriteFilename = path.basename(sliceData.retinaImgDest),
-						bgWidth = Math.floor(retinaSpriteImgData.properties.width / 2),
-						spriteImg = path.join(options.spritepath + spriteFilename) + sliceData.destImgStamp;
+						retinaSpriteImg = sliceData.retinaSpriteImg,
+						bgWidth = Math.floor(retinaSpriteImgData.properties.width / 2);
 
-						var retinaCss = '\n\n/* '+ spriteImg + ' */\n';
-
-						// http://w3ctech.com/p/1430
-						retinaCss += '@media only screen and (-o-min-device-pixel-ratio: 3/2), only screen and (min--moz-device-pixel-ratio: 1.5), only screen and (-webkit-min-device-pixel-ratio: 1.5), only screen and (min-resolution: 240dpi), only screen and (min-resolution: 2dppx) {';
-
-						retinaCss += '\n  ';
-						retinaCss += cssSelectors.join(',\n  ');
-			            retinaCss += ' { background-image:url('+ spriteImg +'); background-size:' + bgWidth + 'px auto;}';
+						var retinaCss = cssSelectors.join(',\n  ');
+						retinaCss += ' { background-image:url('+ retinaSpriteImg +'); background-size:' + bgWidth + 'px auto;}';
 			            retinaCss += '\n  ';
 			            retinaCss += cssProps.join('\n  ');
-			            retinaCss += '\n}\n';
+
+			            retinaCss = options.MEDIA_QUERY_CSS_TMPL.replace(/\{cssText\}/g, retinaCss)
+			            	.replace(/\{imgDest\}/g, retinaSpriteImg);
 
 			            sliceData.cssData += retinaCss;
 					}
